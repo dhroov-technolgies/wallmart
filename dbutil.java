74d59f7affa2245c6242de43b5f72606bb7d37a7
amarmudiraj <amarmudiraj@gmail.com>
2020-08-24 07:44:28 +0530

    update dbutil

--- dbutil.java
+++ dbutil.java
@@ -1,2 +1,4 @@
 heloo this is  my java code i have writeeen
-this is the udpdate made for second commit
+this is the udpdate made for second commi
+Hellow i am writing this file from the develpment branch
+t
